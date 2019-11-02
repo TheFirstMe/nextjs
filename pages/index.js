2450393bfd2613ccb5f77c59f60a18897850cce0
Sourabh <sourabhsubhod@gmail.com>
2019-11-03 01:27:43 +0530

    navigate between pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Link from 'next/link';
+
 const Index = () => (
     <div>
+        <Link href="/about" >
+            <a title="About page">About page</a>
+        </Link>
         <p>Hello World!</p>
     </div>
 );
